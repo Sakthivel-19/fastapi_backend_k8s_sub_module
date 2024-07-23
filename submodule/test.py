c9d0772b370f9dae7ee127ac8203cfe7360687db
Sakthivel-19 <111050238+Sakthivel-19@users.noreply.github.com>
2024-07-23 17:12:53 +0530

    removed unnecessary self parameter

--- submodule/test.py
+++ submodule/test.py
@@ -1,9 +1,9 @@
 class Test:
 
-    def test_function(self):
+    def test_function():
 
         return 'Test function'
     
-    def test_function_copy(self):
+    def test_function_copy():
 
         return 'Test function copy'
